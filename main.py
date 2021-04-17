a2e2c7c4af8243bfe3ff0e3723df4cd7a9801f09
nicolaslopez1000 <nicolaslopez1000@hotmail.com>
2021-04-17 20:39:49 -0300

    Just format changes.

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ import datetime
 
 load_dotenv()
 
+
 class PrometeoAPI:
-    def __init__(self,user,pwd):
+    def __init__(self, user, pwd):
         self.base_url = 'https://prometeoapi.com'
         self.session = requests.Session()
         self.__user = user
         self.__pwd = pwd
         self._login()
 
-
-    def _generate_csrf_token(self,url):
+    def _generate_csrf_token(self, url):
         '''
         This function gets the csrf token from the login page needed to
         do request in order log into the website
@@ -40,19 +40,19 @@ class PrometeoAPI:
         ammount of requests of the month, data available from the dashboard recieved after the log in
         '''
 
-        url = f"{self.base_url}/dashboard/login/"
+        url = f'{self.base_url}/dashboard/login/'
 
         csrf = self._generate_csrf_token(url)
 
-        payload = {'csrfmiddlewaretoken': csrf,
-                   'username': self.__user,
-                   'password': self.__pwd}
+        payload = {
+            'csrfmiddlewaretoken': csrf,
+            'username': self.__user,
+            'password': self.__pwd
+        }
 
-        response = self.session.request("POST", url, data=payload)
-
+        response = self.session.request('POST', url, data=payload)
 
         tree = html.fromstring(response.content)
-
 
         page_title_element = tree.xpath("//title")[0]
         page_title = str(page_title_element.text_content()).strip()
@@ -62,14 +62,15 @@ class PrometeoAPI:
             error_msj = self._strip_text(error)
             raise Exception(f'Failed to log into the site, response text: {error_msj}')
 
-        username_element = tree.xpath("//nav//*[contains(@class,'login-info__data')]/p[contains(@class,'text-white')]")[0]
+        username_element = tree.xpath("//nav//*[contains(@class,'login-info__data')]/p[contains(@class,'text-white')]")[
+            0]
         self.username = self._strip_text(username_element)
 
         api_key_element = tree.xpath("//p[contains(@class,'api-key-field')]")[0]
         self.api_key = self._strip_text(api_key_element)
 
-        #requests_mes_element = tree.xpath("//p[contains(.,'Requests este mes:')]/b")[0]
-        #self.requests_mes = str(requests_mes_element.text_content()).strip()
+        # requests_mes_element = tree.xpath("//p[contains(.,'Requests este mes:')]/b")[0]
+        # self.requests_mes = str(requests_mes_element.text_content()).strip()
 
     def get_requests_current_month(self):
 
@@ -82,23 +83,22 @@ class PrometeoAPI:
             json_table = response.json()
             return json_table.get('usage_table')
 
-
     def refresh_api_key(self):
         csrf = self._generate_csrf_token('https://prometeoapi.com/dashboard/')
         headers = {'X-CSRFToken': csrf}
 
         request_url = 'https://prometeoapi.com/dashboard/reset-key/'
-        response = self.session.post(request_url,headers=headers)
+        response = self.session.post(request_url, headers=headers)
         self.api_key = response.json().get('api_key')
 
         return self.api_key
 
-    def _strip_text(self,element):
+    def _strip_text(self, element):
         return str(element.text_content()).strip()
 
 
 if __name__ == '__main__':
-    api = PrometeoAPI(user=os.environ.get('PROMETEO_USERNAME'),pwd=os.environ.get('PROMETEO_PASSWORD'))
+    api = PrometeoAPI(user=os.environ.get('PROMETEO_USERNAME'), pwd=os.environ.get('PROMETEO_PASSWORD'))
 
     print(api.api_key)
     print(api.username)
